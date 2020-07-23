7206aeb83158352cdf5ef660203582d878d4dffb
Rohit Patil <rahulhimesh09@gmail.com>
2020-07-23 15:45:53 +0530

    setup.py revamp with more metadata

--- setup.py
+++ setup.py
@@ -23,5 +23,18 @@ setup(name="tracklist2playlist",
       entry_points={
           "console_scripts": [
               "t2t = tracklist2playlist.main:main", ]
-          }
+          },
+      license="MIT",
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Topic :: Multimedia :: Sound/Audio",
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          ]
       )
